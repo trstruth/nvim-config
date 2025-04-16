a67a69072328121052c732767542441fdc0593cb
Tristan Struthers <trstruthers@microsoft.com>
2025-04-16 14:32:38 +0000

    Make filename display relative path

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,15 +1,24 @@
 return {
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" }, -- optional but looks better
-		config = function()
-			require("lualine").setup({
-				options = {
-					theme = "auto", -- Uses your colorscheme
-					section_separators = "",
-					component_separators = "",
-				},
-			})
-		end,
-	},
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" }, -- optional but looks better
+        config = function()
+            require("lualine").setup({
+                options = {
+                    theme = "auto", -- Uses your colorscheme
+                    section_separators = "",
+                    component_separators = "",
+                    icons_enabled = true,
+                },
+                sections = {
+                    lualine_c = {
+                        {
+                            "filename",
+                            path = 1,
+                        }
+                    }
+                }
+            })
+        end,
+    },
 }
